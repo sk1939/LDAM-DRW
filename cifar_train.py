25d09aba13a20100465769c793990e0435e71fe7
sk1939 <stevekottackal@gmail.com>
2021-11-29 01:33:59 -0500

    made required changes

--- cifar_train.py
+++ cifar_train.py
@@ -150,8 +150,8 @@ def main_worker(gpu, ngpus_per_node, args):
         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
     ])
 
-    train_dataset = IMBALANCECIFAR100(root='./data', imb_type=args.imb_type, imb_factor=args.imb_factor, rand_number=args.rand_number, train=True, download=True, transform=transform_train)
-    val_dataset = datasets.CIFAR100(root='./data', train=False, download=True, transform=transform_val)
+    train_dataset = mn.labelArray
+    val_dataset = mn.imbalanced_data
     cls_num_list = train_dataset.get_cls_num_list()
     print('cls num list:')
     print(cls_num_list)
