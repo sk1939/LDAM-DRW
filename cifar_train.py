12ce3ab1d33554051582c05da117a82380fe439f
sk1939 <stevekottackal@gmail.com>
2021-11-29 12:53:05 -0500

    made required changes

--- cifar_train.py
+++ cifar_train.py
@@ -102,7 +102,7 @@ def main_worker(gpu, ngpus_per_node, args):
 
     # create model
     print("=> creating model '{}'".format(args.arch))
-    num_classes = 100
+    num_classes = 10
     use_norm = True if args.loss_type == 'LDAM' else False
     model = models.__dict__[args.arch](num_classes=num_classes, use_norm=use_norm)
 
@@ -150,8 +150,8 @@ def main_worker(gpu, ngpus_per_node, args):
         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
     ])
 
-    train_dataset = mn.labelArray
-    val_dataset = mn.imbalanced_data
+    train_dataset = IMBALANCECIFAR10(root='./data', imb_type=args.imb_type, imb_factor=args.imb_factor, rand_number=args.rand_number, train=True, download=True, transform=transform_train)
+    val_dataset = datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_val)
     cls_num_list = train_dataset.get_cls_num_list()
     print('cls num list:')
     print(cls_num_list)
